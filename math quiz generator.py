83d33e8fa1ae973d3db90fb1573fa9e9cf72cf13
SUHAS SREENATH IYENGAR <suhassiyengar@gmail.com>
2025-07-12 11:55:52 +0530

    Update math quiz generator.py with leadboard feature

--- math quiz generator.py
+++ math quiz generator.py
@@ -3,6 +3,17 @@ import time
 
 TOTAL_QUESTIONS=10
 
+def storage_ofusers():
+    with open("player.txt","a") as f:
+        f.write(name+":"+str(total_time)+"seconds.\n")
+
+def leadboard_():
+    print("\n📊 Leaderboard:")
+    print("-" * 25)
+    with open("player.txt","r") as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
 
 def question_():
     OPERATORS=["+","-","*"]
@@ -28,14 +39,31 @@ def user_interface():
                 print("Your Answer is wrong TRY AGAIN! TIME'S RUNNING")
             wrong+=1
     return wrong
-name=input("ENTYER UR NAME: ")
+
+name=input("ENTER UR NAME: ")
 START=input("PRESS ENTER TO START THE GAME:")
 
 start_time=time.time()
 wrong=user_interface()
 end_time=time.time()
 total_time=round(end_time-start_time,2)
+storage_ofusers()
 print("HURRAY!",name,"You Finished The Quiz With ",wrong,"errors in ",total_time,"SECONDS")
+f=input("do u wanna see the previous plays(y):")
+if f.lower()!="y":
+    print("Thank you for playing!.")
+
+else:
+    leadboard_()
+
+
+
+
+
+
+
+
+
 
 
 
